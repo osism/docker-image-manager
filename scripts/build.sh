cd81597c66cd8b0f2852ddf867d788a29387958c
Christian Berendt <berendt@betacloud-solutions.de>
2020-11-26 16:10:40 +0100

    Fix issues in the build/push scripts
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,7 +9,6 @@ set -x
 # OPENSTACK_VERSION
 # RELEASE_OSISM
 # REPOSITORY
-# VERSION
 # VERSION_AWX
 
 # Set default values
@@ -22,7 +21,6 @@ OPENSTACK_VERSION=${OPENSTACK_VERSION:-ussuri}
 RELEASE_OSISM=${RELEASE_OSISM:-latest}
 REPOSITORY=${REPOSITORY:-osism/manager}
 REVISION=$(git rev-parse --short HEAD)
-VERSION=${VERSION:-latest}
 VERSION_AWX=${VERSION_AWX:-latest}
 
 
@@ -36,7 +34,7 @@ docker build \
     --build-arg "RELEASE_CEPH=$CEPH_VERSION" \
     --build-arg "RELEASE_OPENSTACK=$OPENSTACK_VERSION" \
     --build-arg "RELEASE_OSISM=$RELEASE_OSISM" \
-    --tag "$REPOSITORY:$VERSION" \
+    --tag "$REPOSITORY:$CEPH_VERSION-$OPENSTACK_VERSION" \
     --label "org.opencontainers.image.created=$CREATED" \
     --label "org.opencontainers.image.documentation=https://docs.osism.de" \
     --label "org.opencontainers.image.licenses=ASL 2.0" \
@@ -45,6 +43,6 @@ docker build \
     --label "org.opencontainers.image.title=kolla-ansible" \
     --label "org.opencontainers.image.url=https://www.osism.de" \
     --label "org.opencontainers.image.vendor=Betacloud Solutions GmbH" \
-    --label "org.opencontainers.image.version=$VERSION" \
+    --label "org.opencontainers.image.version=$CEPH_VERSION-$OPENSTACK_VERSION" \
     --no-cache \
     $BUILD_OPTS .
