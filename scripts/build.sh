1220cefa9728c935aa3656732f10ccbddc13d54a
Christian Berendt <berendt@betacloud-solutions.de>
2020-11-26 15:11:30 +0100

    Fix build/push scripts
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,35 +4,37 @@ set -x
 # Available environment variables
 #
 # BUILD_OPTS
+# CEPH_VERSION
 # DOCKER_REGISTRY
+# OPENSTACK_VERSION
+# RELEASE_OSISM
 # REPOSITORY
 # VERSION
 # VERSION_AWX
-# RELEASE_CEPH
-# RELEASE_OPENSTACK
-# RELEASE_OSISM
 
 # Set default values
 
 BUILD_OPTS=${BUILD_OPTS:-}
+CEPH_VERSION=${CEPH_VERSION:-nautilus}
 CREATED=$(date --rfc-3339=ns)
 DOCKER_REGISTRY=${DOCKER_REGISTRY:-quay.io}
-RELEASE_CEPH=${RELEASE_CEPH:-nautilus}
-RELEASE_OPENSTACK=${RELEASE_OPENSTACK:-train}
+OPENSTACK_VERSION=${OPENSTACK_VERSION:-ussuri}
 RELEASE_OSISM=${RELEASE_OSISM:-latest}
 REPOSITORY=${REPOSITORY:-osism/manager}
 REVISION=$(git rev-parse --short HEAD)
 VERSION=${VERSION:-latest}
 VERSION_AWX=${VERSION_AWX:-latest}
 
+
 if [[ -n $DOCKER_REGISTRY ]]; then
     REPOSITORY="$DOCKER_REGISTRY/$REPOSITORY"
 fi
 
+
 docker build \
     --build-arg "VERSION_AWX=$VERSION_AWX" \
-    --build-arg "RELEASE_CEPH=$RELEASE_CEPH" \
-    --build-arg "RELEASE_OPENSTACK=$RELEASE_OPENSTACK" \
+    --build-arg "RELEASE_CEPH=$CEPH_VERSION" \
+    --build-arg "RELEASE_OPENSTACK=$OPENSTACK_VERSION" \
     --build-arg "RELEASE_OSISM=$RELEASE_OSISM" \
     --tag "$REPOSITORY:$VERSION" \
     --label "org.opencontainers.image.created=$CREATED" \
