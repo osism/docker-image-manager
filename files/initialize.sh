6bbdf55ddea61ab27e4e6f3c099e6d95fb35b1f5
Christian Berendt <berendt@betacloud-solutions.de>
2020-12-19 23:36:27 +0100

    Remove custom logo (#7)
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- files/initialize.sh
+++ files/initialize.sh
@@ -10,5 +10,3 @@ ANSIBLE_REMOTE_TEMP=/tmp ANSIBLE_LOCAL_TEMP=/tmp ansible-playbook -i "127.0.0.1,
 
 awx --conf.host http://awx-web:8052 setting modify AWX_ANSIBLE_CALLBACK_PLUGINS '["/usr/local/lib/python3.6/site-packages/ara/plugins/callback"]'
 awx --conf.host http://awx-web:8052 setting modify TOWER_URL_BASE http://$AWX_SERVER_HOST:$AWX_SERVER_PORT
-
-python3 /home/awx/set-custom-logo.py
