2ef2d6ff7cfe9943d03a0d0e23f1ddb56a67ea4e
Christian Berendt <berendt@betacloud-solutions.de>
2020-12-19 23:14:46 +0100

    Make ceph init optional (#6)
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- files/initialize.sh
+++ files/initialize.sh
@@ -6,7 +6,7 @@
 #     -d '{"CUSTOM_VENV_PATHS": ["/opt/venvs/ceph/", "/opt/venvs/kolla/", "/opt/venvs/osism/"]}' \
 #     -H 'Content-Type:application/json'
 
-ANSIBLE_REMOTE_TEMP=/tmp ANSIBLE_LOCAL_TEMP=/tmp ansible-playbook -i "127.0.0.1," -c local /opt/ansible/awx.yml
+ANSIBLE_REMOTE_TEMP=/tmp ANSIBLE_LOCAL_TEMP=/tmp ansible-playbook -i "127.0.0.1," -c local -e ceph=$CEPH /opt/ansible/awx.yml
 
 awx --conf.host http://awx-web:8052 setting modify AWX_ANSIBLE_CALLBACK_PLUGINS '["/usr/local/lib/python3.6/site-packages/ara/plugins/callback"]'
 awx --conf.host http://awx-web:8052 setting modify TOWER_URL_BASE http://$AWX_SERVER_HOST:$AWX_SERVER_PORT
