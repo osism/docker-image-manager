51fbce177a17fc3329b956546371ccab4016bb97
Christian Berendt <berendt@betacloud-solutions.de>
2021-04-10 14:46:25 +0200

    Use python 3.8
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- files/initialize.sh
+++ files/initialize.sh
@@ -8,5 +8,5 @@
 
 ANSIBLE_REMOTE_TEMP=/tmp ANSIBLE_LOCAL_TEMP=/tmp ansible-playbook -i "127.0.0.1," -c local -e ceph=$CEPH /opt/ansible/awx-bootstrap.yml
 
-awx --conf.host http://awx-web:8052 setting modify AWX_ANSIBLE_CALLBACK_PLUGINS '["/usr/local/lib/python3.6/site-packages/ara/plugins/callback"]'
+awx --conf.host http://awx-web:8052 setting modify AWX_ANSIBLE_CALLBACK_PLUGINS '["/usr/local/lib/python3.8/site-packages/ara/plugins/callback"]'
 awx --conf.host http://awx-web:8052 setting modify TOWER_URL_BASE http://$AWX_SERVER_HOST:$AWX_SERVER_PORT
