df08a58d5ece84964093d92de805274a4d230a72
Christian Berendt <berendt@betacloud-solutions.de>
2021-04-12 12:55:03 +0200

    Cleanup initialize.sh
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- files/initialize.sh
+++ files/initialize.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# NOTE: not required when placing venvs in /var/lib/awx/venv
-#
-# curl -X PATCH "http://$AWX_ADMIN_USER:$AWX_ADMIN_PASSWORD@localhost:8052/api/v2/settings/system/" \
-#     -d '{"CUSTOM_VENV_PATHS": ["/opt/venvs/ceph/", "/opt/venvs/kolla/", "/opt/venvs/osism/"]}' \
-#     -H 'Content-Type:application/json'
-
 ANSIBLE_REMOTE_TEMP=/tmp ANSIBLE_LOCAL_TEMP=/tmp ansible-playbook -i "127.0.0.1," -c local -e ceph=$CEPH /opt/ansible/awx-bootstrap.yml
 
 awx --conf.host http://awx-web:8052 setting modify AWX_ANSIBLE_CALLBACK_PLUGINS '["/usr/local/lib/python3.8/site-packages/ara/plugins/callback"]'
