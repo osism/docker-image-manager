fe94c8dc6c688a307f08dac8f2c49416546432a2
Christian Berendt <berendt@betacloud-solutions.de>
2021-04-12 18:05:28 +0200

    Set ansible_python_interpreter=/usr/bin/python3 in initialize script
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- files/initialize.sh
+++ files/initialize.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-ANSIBLE_REMOTE_TEMP=/tmp ANSIBLE_LOCAL_TEMP=/tmp ansible-playbook -i "127.0.0.1," -c local -e ceph=$CEPH /opt/ansible/awx-bootstrap.yml
+ANSIBLE_REMOTE_TEMP=/tmp ANSIBLE_LOCAL_TEMP=/tmp ansible-playbook -i "127.0.0.1," -c local -e ceph=$CEPH -e ansible_python_interpreter=/usr/bin/python3 /opt/ansible/awx-bootstrap.yml
 
 awx --conf.host http://awx-web:8052 setting modify AWX_ANSIBLE_CALLBACK_PLUGINS '["/usr/local/lib/python3.8/site-packages/ara/plugins/callback"]'
