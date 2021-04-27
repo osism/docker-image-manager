fef54b07c872df44b44c54c670898672044f53ab
Christian Berendt <berendt@betacloud-solutions.de>
2021-04-27 19:46:03 +0200

    Fix initialize.sh script (#62)
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- files/initialize.sh
+++ files/initialize.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+source /etc/tower/conf.d/environment.sh
+
+export TOWER_USERNAME
+export TOWER_PASSWORD
+export TOWER_HOST
+
 ANSIBLE_REMOTE_TEMP=/tmp ANSIBLE_LOCAL_TEMP=/tmp ansible-playbook -i "127.0.0.1," -c local -e ansible_python_interpreter=/usr/bin/python3 /opt/ansible/awx-wait.yml
 
-awx --conf.host http://awx:8052 login -f human
-awx --conf.host http://awx:8052 setting modify AWX_ANSIBLE_CALLBACK_PLUGINS '["/usr/local/lib/python3.8/site-packages/ara/plugins/callback"]'
+awx login
+awx setting modify AWX_ANSIBLE_CALLBACK_PLUGINS '["/usr/local/lib/python3.8/site-packages/ara/plugins/callback"]'
