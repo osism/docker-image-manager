6bc7c0c20bbdd695ed7bae8d0be16c30d3d0fa9b
Christian Berendt <berendt@betacloud-solutions.de>
2021-05-13 13:30:03 +0200

    Overall improvements (#69)
    * use configuration volume
    * add crond service
    * add sync script
    * remove overlays
    
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

---
+++ files/update.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+for environment in ceph kolla generic infrastructure monitoring custom openstack; do
+    mkdir -p /configuration/$environment
+    pushd /configuration/$environment > /dev/null
+
+    rsync -aL /configuration.pre/$environment/ /configuration/$environment/
+
+    case "$environment" in
+        ceph)
+            rsync -a /interface/ceph-ansible/ /configuration/$environment/ ;;
+
+        kolla)
+            rsync -a /interface/kolla-ansible/ /configuration/$environment/ ;;
+        
+        generic)
+        infrastructure)
+        monitoring)
+        custom)
+        openstack)
+            rsync -a /interface/osism-ansible/$environment-* /configuration/$environment/ ;;
+    esac 
+
+    if [[ ! -e .git ]]; then
+        git init
+        git config user.name "Configuration Reconciler"
+        git config user.email "configuration@reconciler.local"
+
+        git add -A
+        git commit -m $(date +"%Y-%m-%d-%H-%M")
+    else
+        CHANGED=$(git diff --exit-code)
+        if [[ $? -gt 0 ]]; then
+            git add -A
+            git commit -m $(date +"%Y-%m-%d-%H-%M")
+        fi
+    fi
+
+    popd > /dev/null
+done
